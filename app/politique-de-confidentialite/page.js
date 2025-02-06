e63206f5e8dd750af05936328574d70a8f6bfbb0
mowebiaz <mowebiaz@gmail.com>
2025-02-06 15:12:35 +0100

    Add privacy policy

--- app/politique-de-confidentialite/page.js
+++ app/politique-de-confidentialite/page.js
@@ -1,14 +1,17 @@
+import { ConfidentialityContent } from './ConfidentialityContent/ConfidentialityContent'
+
 export const metadata = {
-  title: "Politique de confidentialité",
+  title: 'Politique de confidentialité',
   alternates: {
     canonical: 'https://avoriazloc.fr/politique-de-confidentialite',
-  }
+  },
 }
 
 export default function PolitiqueDeConfidentialite() {
   return (
     <main className="confidentiality">
       <h1>Politique de Confidentialité</h1>
+      <ConfidentialityContent />
     </main>
   )
 }
